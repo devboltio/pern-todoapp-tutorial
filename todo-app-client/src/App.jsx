bf4bc6dd567945e48a1623c7a9c6fcd7318ee85b
Alex Ficachi <alexficachirizo@gmail.com>
2024-09-05 14:17:27 -0700

    Add Create, Update, & Delete functionality to React App using existing express endpoints

--- todo-app-client/src/App.jsx
+++ todo-app-client/src/App.jsx
@@ -3,27 +3,68 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const HOST = "http://localhost:3000/todos";
+  const HOST = "http://localhost:3000/todos/";
   const [state, setState] = useState([]);
+  const [input, setInput] = useState("");
+
+  const fetchTodos = async () => {
+    const response = await axios.get(HOST);
+    const todos = response.data;
+    todos.sort((a, b) => {
+      return a.id - b.id;
+    });
+    setState(todos);
+  };
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      const response = await axios.get(HOST);
-      setState(response.data);
-    };
     fetchTodos();
   }, []);
 
+  const addTodo = async () => {
+    const response = await axios.post(HOST, { title: input });
+    setState((state) => [...state, response.data]);
+    setInput("");
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addTodo();
+  };
+
+  const toggleComplete = async (todo) => {
+    const { id, completed } = todo;
+    await axios.put(HOST + id, { completed: !completed });
+    await fetchTodos();
+  };
+
+  const deleteTodo = async (id) => {
+    await axios.delete(HOST + id);
+    await fetchTodos();
+  };
+
   const todos = state.map((todo) => {
     return (
-      <div>
-        {todo.title}
-        {" - " + (todo.completed ? "completed" : "incomplete")}
+      <div key={todo.id}>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => toggleComplete(todo)}
+        />
+        {todo.title}{" "}
+        <button onClick={() => deleteTodo(todo.id)}> DELETE </button>
       </div>
     );
   });
 
-  return <>{todos}</>;
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <input type="submit" />
+      </form>
+      {todos}
+    </>
+  );
 }
 
 export default App;
